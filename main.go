6e0907f759b6fac6147c8394c479d13804442890
Shuvro <shuvro@Shuvros-MacBook-Pro.local>
2022-01-25 04:03:06 +0600

    max value find

--- main.go
+++ main.go
@@ -3,35 +3,54 @@ package main
 import "fmt"
 
 func main() {
-	var items [7]int
+	var items [6]int
 
-	var search int
+	var max int
 
 	var value int
-	var frist = 0
-	var last = len(items) - 1
-	var middle = (frist + last) / 2
-	fmt.Println("Enter a Value of Search: ")
-	fmt.Scan(&search)
+
 	for i := 0; i < len(items); i++ {
 		fmt.Scan(&value)
 		items[i] = value
 	}
 
-	for frist <= last {
-		if items[middle] < search {
-			frist = middle + 1
-			middle = (frist + last) / 2
-		} else if items[middle] == search {
-			fmt.Println("Found the position at: ", middle)
-			break
-		} else {
-			last = middle - 1
-			middle = (frist + last) / 2
+	for i := 0; i < len(items); i++ {
+		for j := i + 1; j < len(items); j++ {
+
+			if items[i] < items[j] {
+				max = items[j]
+
+			} else if items[i] == items[j] {
+				max = items[j]
+			} else {
+
+				if max < items[i] {
+					max = items[i]
+				} else {
+					max = max
+				}
+
+			}
+
+		}
+	}
+	shortHand()
+	fmt.Println("max 1", max)
+
+}
+
+func shortHand() {
+	array := [...]int{
+		1, 2, 4, 44, 5, 66, 11,
+	}
+
+	var max int = array[0]
+
+	for i := 0; i < len(array); i++ {
+		if max < array[i] {
+			max = array[i]
 		}
 	}
 
-	if frist > last {
-		fmt.Println("Not Found")
-	}
+	fmt.Println("max 2", max)
 }
