797ee815e7c77c4eb9e028aad36912384980a23b
Shuvro <shuvro@Shuvros-MacBook-Pro.local>
2022-01-27 04:00:47 +0600

    swap without variable

--- main.go
+++ main.go
@@ -3,47 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Bubble Sort")
-	var items [5]int
-	var value int
-	for i := 0; i < len(items); i++ {
-		fmt.Scan(&value)
-		items[i] = value
-	}
-	for a := 0; a < len(items)-1; a++ {
-		for b := 0; b < len(items)-1; b++ {
-			if items[b] > items[b+1] {
-				var temp int
-				temp = items[b]
-				items[b] = items[b+1]
-				items[b+1] = temp
-			}
-		}
-	}
-	fmt.Println(items)
-	var search int
-	fmt.Println("Enter search value:")
-	fmt.Scan(&search)
-	var frist = 0
-	var last = len(items) - 1
-	var middle = (frist + last) / 2
-	for j := 0; j < len(items); j++ {
+	fmt.Println("Swap Without Variable")
+	var a = 10
+	var b = 20
 
-		if items[middle] < search {
-			frist = middle + 1
-			middle = (frist + last) / 2
-		} else if items[middle] == search {
-			fmt.Print("Position", middle)
-			break
-		} else {
-			last = middle - 1
-			middle = (frist + last) / 2
-		}
+	a = a + b
+	b = a - b
+	a = a - b
 
-	}
-	if frist > last {
-
-		fmt.Println("Not Found")
-	}
-
+	fmt.Println("a is ", a)
+	fmt.Println("b is ", b)
 }
